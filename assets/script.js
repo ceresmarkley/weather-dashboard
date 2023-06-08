d2a942580ce7f4638a8a2a8d101a22d14b17c9c0
markley man <129554518+ceresmarkley@users.noreply.github.com>
2023-06-08 14:35:27 -0400

    added root html content and began API creation in JS

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+$(function () {
+    const apiKey = '02045eae6d8bdfcc80db71ad84fd66e3'; //OpenWeahter API Key
+    const historyEl = $("#history");
+    const searchInputEl = $("#searchInput");
+    const locateBtnEl = $("#locateBtn"); // Locate button element
+  
